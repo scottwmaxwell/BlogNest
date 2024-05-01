17f423bcdaf0b5e384b6bb957b935e835d0a6133
Joseph <josephcrosby1996@gmail.com>
2024-05-01 15:06:17 -0700

    Added files

---
+++ express-app/src/routes/postRoutes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getAllPosts, getPostById, createPost, updatePost, deletePost } from '../controllers/postController';
+
+const router = express.Router();
+
+router.get('/', getAllPosts);
+router.get('/:id', getPostById);
+router.post('/', createPost);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
+
+export default router;
